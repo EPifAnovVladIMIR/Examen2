a2ffa4e47cf98a3d6f836a41f8ae642114e4d340
Vladimir Epifanov <bignasa@list.ru>
2023-04-26 09:09:10 +0300

    Создал Программу

---
+++ Epifanov_program/Epifanov_program/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epifanov_program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+
+}
